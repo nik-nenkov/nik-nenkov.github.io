ee730882db24e857bd3e1e47386fbdf141ba42b0
Nik <nenkov.info@gmail.com>
2022-07-28 21:27:07 +0300

    reformat after many years

--- sketch.js
+++ sketch.js
@@ -1,130 +1,118 @@
-var s;
-var scl = 16;
-var food;
+import "p5.min.js";
+
+let s;
+const scl = 16;
+let food;
 
 function setup() {
-	frameRate(8);
-	createCanvas(400, 400);
-	s = new Snake();
-	pickLocation();
-}
-
-function pickLocation(){
-	var cols = floor(width/scl);
-	var rows = floor(height/scl);
-	food = createVector(floor(random(cols)),floor(random(rows)));
-	food.mult(scl);
+    frameRate(8);
+    createCanvas(400, 400);
+    s = new Snake();
+    pickLocation();
 }
 
 function draw() {
-	background(105,105,105);
-	s.death();
-	s.update();
-	s.show();
-	fill(255,145,90);
-	rect(food.x,food.y,scl,scl);
-	if(s.eat(food)){
-		pickLocation();
-	}
-}
-
-function keyPressed(){
-	if (keyCode === UP_ARROW||keyCode === 87){
-		s.dir(0,-1);
-	}else if(keyCode === DOWN_ARROW||keyCode === 83){
-		s.dir(0, 1);
-	}else if(keyCode === RIGHT_ARROW||keyCode === 68){
-		s.dir(1, 0);
-	}else if(keyCode === LEFT_ARROW||keyCode === 65){
-		s.dir(-1,0);
-	}
-	if (mouseIsPressed) {
-	    if (mouseButton === LEFT) {
-	      s.total++;
-	  }
+    background(105, 105, 105);
+    s.death();
+    s.update();
+    s.show();
+    fill(255, 145, 90);
+    rect(food.x, food.y, scl, scl);
+    if (s.eat(food)) {
+        pickLocation();
     }
 }
 
-// function touchMoved(){
-// 	var optX=mouseX/200;
-// 	var optY=mouseY/200;
-// 	if (optX<1&&optY<1){
-// 		s.dir(0,-1);
-// 	}else if(optX>=1&&optY<1){
-// 		s.dir(0, 1);
-// 	}else if(optX<1&&optY>=1){
-// 		s.dir(1, 0);
-// 	}else if(optX>=1&&optY>=1){
-// 		s.dir(-1,0);
-// 	}
-// 	if (mouseIsPressed) {
-// 		if (mouseButton === LEFT) {
-// 			s.total++;
-// 		}
-// 	}
-// }
+function keyPressed() {
+    if (Event.keyCode === KeyboardEvent.UP_ARROW || Event.keyCode === 87) {
+        s.dir(0, -1);
+    } else if (Event.keyCode === KeyboardEvent.DOWN_ARROW || Event.keyCode === 83) {
+        s.dir(0, 1);
+    } else if (Event.keyCode === KeyboardEvent.RIGHT_ARROW || Event.keyCode === 68) {
+        s.dir(1, 0);
+    } else if (Event.keyCode === KeyboardEvent.LEFT_ARROW || Event.keyCode === 65) {
+        s.dir(-1, 0);
+    }
+    if (Event.mouseIsPressed) {
+        if (Event.mouseButton === KeyboardEvent.LEFT) {
+            s.total++;
+        }
+    }
+}
 
-function Snake(){
+function Snake() {
 
-	this.x=0;
-	this.y=0;
-	this.xspeed = 1;
-	this.yspeed = 0;
-	this.tail 	= [];
-	this.total 	= 0;
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.tail = [];
+    this.total = 0;
 
-	this.dir = function(x,y){
-		this.xspeed=x;
-		this.yspeed=y;
-	}
+    this.dir = function (x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
 
-	this.eat = function(pos){
-		var d = dist(this.x,this.y,pos.x,pos.y);
-		if(d<scl){this.total++;return true;}else{return false;}
-	}
+    this.eat = function (pos) {
+        const d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < scl) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	this.death = function(){
-		for(var i =0; i<this.tail.length; i++){
-			var pos = this.tail[i];
-			var d = dist(this.x,this.y,pos.x,pos.y);
-			if (d<scl){
-				this.total=0;
-				this.tail=[];
-				console.log('starting over');
-			}
-		}
-	}
+    this.death = function () {
+        for (let i = 0; i < this.tail.length; i++) {
+            const pos = this.tail[i];
+            const d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < scl) {
+                this.total = 0;
+                this.tail = [];
+                console.log('starting over');
+            }
+        }
+    }
 
-	this.update = function(){
-		if(this.total===this.tail.length){
-			for(var i =0;i<this.tail.length-1;i++){
-				this.tail[i]=this.tail[i+1];
-			}
-		}
-		
-		this.tail[this.total-1]=createVector(this.x,this.y);
-		
-		this.x=this.x+this.xspeed*scl;
-		this.y=this.y+this.yspeed*scl;
-		if(this.x>width-scl){
-			this.x=0;
-		}
-		if(this.x<0){
-			this.x=width;
-		}
-		if(this.y>height-scl){
-			this.y=0;
-		}
-		if(this.y<0){
-			this.y=height;
-		}
-	}
+    this.update = function () {
+        if (this.total === this.tail.length) {
+            for (let i = 0; i < this.tail.length - 1; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
 
-	this.show = function(){
-		fill(155,255,155);
-		for(var i =0;i<this.tail.length;i++){
-			rect(this.tail[i].x,this.tail[i].y,scl,scl);
-		}
-		rect(this.x,this.y,scl,scl);
-	}
+        this.tail[this.total - 1] = Math.createVector(this.x, this.y);
+
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+        if (this.x > width - scl) {
+            this.x = 0;
+        }
+        if (this.x < 0) {
+            this.x = width;
+        }
+        if (this.y > height - scl) {
+            this.y = 0;
+        }
+        if (this.y < 0) {
+            this.y = height;
+        }
+    }
+
+    this.show = function () {
+        fill(155, 255, 155);
+        for (let i = 0; i < this.tail.length; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+        rect(this.x, this.y, scl, scl);
+    }
+}
+
+function pickLocation() {
+    const cols = Math.floor(width / scl);
+    const rows = Math.floor(height / scl);
+    food = Math.createVector(Math.floor(random(cols)), Math.floor(random(rows)));
+    food.mult(scl);
 }
