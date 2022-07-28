2a0990a562398e4f49eeb86995052e2b0a96facb
Nik <nenkov.info@gmail.com>
2022-07-28 23:51:30 +0300

    trying to store scripts locally ;(

--- scripts/map.js
+++ scripts/map.js
@@ -54,6 +54,14 @@ $.getJSON(districtsJSONPath, function (data) {
         }
     }).addTo(map);
 })
+const blueIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 const greenIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -105,6 +113,7 @@ function onEachFeature(feature, layer) {
         layer.on('clickout', function (e) {
             this.closePopup();
         });
+        layer.setIcon(blueIcon);
         if (feature.properties.n_properties > 2) {
             layer.setIcon(greenIcon);
         }
